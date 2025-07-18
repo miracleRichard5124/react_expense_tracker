e19456cb0c35f3f90c1c1d6278b1e04f4e470124
Miracle <miraclerichard5124@gmail.com>
2025-07-18 23:41:08 +0100

    Fixed the Layout for the Login Page

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
 
   return (
     <AuthLayout>
-      <div>
-        <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <div >
           <h3 className="text-xl font-semibold text-black">Welcome Back!</h3>
           <p className="text-xs text-slate-700 mt-[5px] mb-6">
             Please enter your details to log in
