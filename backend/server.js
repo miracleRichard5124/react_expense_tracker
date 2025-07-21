a21ab71d09840c3c4479e33ad6bb3cb547febf26
Miracle <miraclerichard5124@gmail.com>
2025-07-22 00:00:05 +0100

    set up routes for uploading images

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,7 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
