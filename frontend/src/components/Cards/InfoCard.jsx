3453cb9ff9ae52d8c0fc76bf460745bda0c20d20
Miracle <miraclerichard5124@gmail.com>
2025-08-05 03:12:14 +0100

    Updated the Homepage UI in the Dashboard
    - Worked on the UI to retrieve and display User data
    - Fixed the issue of data fetched from the Database but not displaying on
    the frontend.
    - Created more components and helper functions to properly render data on
    the client-side
    - Achieved displaying Total Balance, Total Income and Total Expense on
      the client-side

---
+++ frontend/src/components/Cards/InfoCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const InfoCard = ({label, value, color, icon}) => {
+  return (
+    <div className='flex bg-white rounded-2xl gap-6 p-6 shadow-md shadow-gray-100 border border-gray-200/50'>
+      <div className={`${color} w-14 h-14 rounded-full flex justify-center items-center text-white text-[26px] drop-shadow-xl`}>
+        {icon}
+      </div>
+      <div>
+        <h2 className="text-sm text-gray-500 mb-1">{label}</h2>
+        <span className="text-[22px]">{value}</span>  
+      </div>
+    </div>
+  );
+}
+
+export default InfoCard;
--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -4,6 +4,11 @@ import { useUserAuth } from "../../hooks/useUserAuth";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { useNavigate } from "react-router-dom";
+import InfoCard from "../../components/Cards/InfoCard";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
+import { addThousandSeperator } from "../../utils/helper";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 
 const Home = () => {
   useUserAuth();
@@ -13,21 +18,22 @@ const Home = () => {
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  const fetchDashboardData = async() => {
-    if(loading) return;
+  const fetchDashboardData = async () => {
+    if (loading) return;
 
     setLoading(true);
     try {
       const response = await axiosInstance.get(
         `${API_PATHS.DASHBOARD.GET_DATA}`
       );
-      
-      if(response.data){
-        setDashboardData(response.data);
+
+      if (response.data) {
+        console.log(response.data);
+        setDashboardData(response.data.data);
       }
     } catch (error) {
       console.log("Something went wrong. Please try again! ", error);
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
@@ -37,9 +43,45 @@ const Home = () => {
     return () => {};
   }, []);
 
+  if (loading || !dashboardData) {
+    return (
+      <DashboardLayout activeMenu="Dashboard">
+        <div className="my-5 mx-auto">Loading...</div>
+      </DashboardLayout>
+    );
+  }
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className="my-5 mx-auto">Home</div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <InfoCard
+            icon={<IoMdCard />}
+            color="bg-primary"
+            label="Total Balance"
+            value={addThousandSeperator(dashboardData?.totalBalance)}
+          />
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            color="bg-green-500"
+            label="Total Income"
+            value={addThousandSeperator(dashboardData?.totalIncome)}
+          />
+          <InfoCard
+            icon={<LuHandCoins />}
+            color="bg-red-500"
+            label="Total Expense"
+            value={addThousandSeperator(dashboardData.totalExpense)}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate('/expense')}
+          />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
