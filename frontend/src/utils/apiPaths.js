4b26d670b048571f27c5048851ec0a8d5179fee9
Miracle <miraclerichard5124@gmail.com>
2025-09-02 13:01:57 +0100

    updated the backend URL

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,31 +1,31 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_PATHS = {
   AUTH: {
-    LOGIN: '/api/v1/auth/login',
-    REGISTER: '/api/v1/auth/register',
-    GET_USER_INFO: '/api/v1/auth/getUser',
-    UPDATE_USER: '/api/v1/auth/updateUser',
+    LOGIN: `${BASE_URL}/api/v1/auth/login`,
+    REGISTER: `${BASE_URL}/api/v1/auth/register`,
+    GET_USER_INFO: `${BASE_URL}/api/v1/auth/getUser`,
+    UPDATE_USER: `${BASE_URL}/api/v1/auth/updateUser`,
   },
   DASHBOARD: {
-    GET_DATA: '/api/v1/dashboard',
+    GET_DATA: `${BASE_URL}/api/v1/dashboard`,
   },
   INCOME: {
-    ADD_INCOME: '/api/v1/income/add',
-    GET_ALL_INCOME: '/api/v1/income/get',
-    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
-    DOWNLOAD_INCOME: `/api/v1/income/downloadExcel`,
+    ADD_INCOME: `${BASE_URL}/api/v1/income/add`,
+    GET_ALL_INCOME: `${BASE_URL}/api/v1/income/get`,
+    DELETE_INCOME: (incomeId) => `${BASE_URL}/api/v1/income/${incomeId}`,
+    DOWNLOAD_INCOME: `${BASE_URL}/api/v1/income/downloadExcel`,
   },
   EXPENSE: {
-    ADD_EXPENSE: '/api/v1/expense/add',
-    GET_ALL_EXPENSE: '/api/v1/expense/get',
-    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE: `/api/v1/expense/downloadExcel`,
+    ADD_EXPENSE: `${BASE_URL}/api/v1/expense/add`,
+    GET_ALL_EXPENSE: `${BASE_URL}/api/v1/expense/get`,
+    DELETE_EXPENSE: (expenseId) => `${BASE_URL}/api/v1/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE: `${BASE_URL}/api/v1/expense/downloadExcel`,
   },
   IMAGE: {
-    UPLOAD_IMAGE: '/api/v1/auth/upload-image',
+    UPLOAD_IMAGE: `${BASE_URL}/api/v1/auth/upload-image`,
   },
   CURRENCY: {
-    GET_ALL_CURRENCIES: '/api/v1/currencies',
-  }
+    GET_ALL_CURRENCIES: `${BASE_URL}/api/v1/currencies`,
+  },
 };
